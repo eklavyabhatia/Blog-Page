321053140a9ea9a9673a28aca28cffe64592156a
eklavyabhatia <eklavyabhatia@gmail.com>
2020-05-08 02:43:01 +0530

    set up the server and the directory structure

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const port = 8080
+
+const app = express()
+
+app.listen(port, function (err) {
+    if (err) {
+        console.log(err)
+        return
+    }
+    console.log('Server is running on port:', port)
+})
