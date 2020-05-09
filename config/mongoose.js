91fb9b72fe17cf7b82aae97e019135e9019ef12f
eklavyabhatia <eklavyabhatia@gmail.com>
2020-05-10 00:29:05 +0530

    set up the db

---
+++ config/mongoose.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/linkster_development')
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'Error connecting to mongodb'))
+
+db.once('open', function () {
+    console.log('Successfully connected to database')
+})
+
+module.exports = db
--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const express = require('express')
 const port = 8080
-const expressLayouts = require('express-ejs-layouts')
 const app = express()
 
-app.use(express.static('./assets'))
+const db = require('./config/mongoose')
+
+const expressLayouts = require('express-ejs-layouts')
 
 app.use(expressLayouts)
 //extract styles and scripts from subpages into the layout
-app.set('layout extractStyles',true)
-app.set('layout extractScripts',true)
+app.set('layout extractStyles', true)
+app.set('layout extractScripts', true)
 
+app.use(express.static('./assets'))
 
-//use express router
-app.use('/', require('./routes'))
 
 //set up the view engine
 app.set('view engine', 'ejs')
 app.set('views', './views')
+
+//use express router
+app.use('/', require('./routes'))
+
 
 app.listen(port, function (err) {
     if (err) {
