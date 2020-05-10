944c1360920ba6513a910c43db628d58c9357550
eklavyabhatia <eklavyabhatia@gmail.com>
2020-05-10 15:24:35 +0530

    set up the signin and signup pages

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,5 +1,25 @@
 module.exports.profile = function (req, res) {
-    return res.render('user_profile',{
-        title:'UserProfile'
+    return res.render('user_profile', {
+        title: 'UserProfile'
     })
+}
+
+module.exports.signin = function (req, res) {
+    return res.render('user_sign_in', {
+        title: 'Linkster | SignIn'
+    })
+}
+
+module.exports.signup = function (req, res) {
+    return res.render('user_sign_up', {
+        title: 'Linkster | SignUp'
+    })
+}
+
+module.exports.create = function (req, res) {
+    //todo
+}
+
+module.exports.createSession = function (req, res) {
+    //todo
 }
--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,7 @@ const router = express.Router()
 const userController = require('../controllers/user_controller')
 
 router.get('/profile', userController.profile)
+router.get('/sign-in',userController.signin)
+router.get('/sign-up',userController.signup)
 
 module.exports = router
