c617955e84255bb59679fa614feaddcea12fe512
eklavyabhatia <eklavyabhatia@gmail.com>
2020-05-15 17:24:42 +0530

    set up the sign out

--- routes/users.js
+++ routes/users.js
@@ -14,4 +14,6 @@ router.post('/create-session', passport.authenticate(
     { failiureRedirect: '/users/sign-in' }
 ), userController.createSession)
 
+router.get('/sign-out',userController.destroySession)
+
 module.exports = router
