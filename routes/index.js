f787d69ad77cd81799a38bbf081be3ee5920889e
eklavyabhatia <eklavyabhatia@gmail.com>
2020-05-20 22:26:21 +0530

    add form to submit post

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
-
+const passport = require('passport')
 const homeController = require('../controllers/home_controller')
 
 console.log('router loaded')
 
-router.get('/', homeController.home)
+router.get('/', passport.checkAuthentication,homeController.home)
 router.use('/users', require('./users'))
 
 module.exports = router
