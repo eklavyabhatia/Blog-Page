1eef104a4a491a081f1a6a906522b5996aac1634
eklavyabhatia <eklavyabhatia@gmail.com>
2020-05-21 23:20:02 +0530

    set up comments schema

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('passport')
 
 const postsController = require('../controllers/posts_controller')
 
-router.post('/create-post', postsController.create)
+router.post('/create-post',passport.checkAuthentication ,postsController.create)
 
 module.exports = router
