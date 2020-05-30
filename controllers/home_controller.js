2b496949ea68a70da54f5e3c8b1adb7a14752bca
eklavyabhatia <eklavyabhatia@gmail.com>
2020-05-30 15:27:40 +0530

    add async await to controllers

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,19 +1,23 @@
 const Post = require('../models/post')
 const User = require('../models/user')
 
-module.exports.home = function (req, res) {
+module.exports.home = async function (req, res) {
     // console.log(req.cookies)
     // res.cookie('userid',26)
-
-    Post.find({}).populate('user').populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    }).exec(function (err, posts) {
-        User.find({}, function (err, users) {
-            return res.render('home', { 'title': 'Home', posts: posts, users: users })
+    try {
+        let posts = await Post.find({}).populate('user').populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
         })
+        let users = await User.find({})
 
-    })
+        return res.render('home', { 'title': 'Home', posts: posts, users: users })
+
+    } catch (err) {
+        console.log(err)
+        return
+    }
+
 }
